a914764682bab894fe7143687f4ae8596def934a
Frostzeichen <68489239+Frostzeichen@users.noreply.github.com>
2023-05-23 13:27:33 +0000

    Moved the part that asks for numerical input.

--- main.c
+++ main.c
@@ -27,43 +27,109 @@ int main(void) {
     char input[10];
     int index = 0;
   
-    // Input numbers to make array.
-    while (1) {
-      printf("Enter a number or 'x' to submit): ");
-      scanf("%s", input);
-      if (input [0] == 'x') {
-        break;
-      }
-      float f = strtof(input, NULL);
-      dataset_array = realloc(dataset_array, sizeof(float) * (index + 1));
-      dataset_array[index] = f;
-      index++;
-    }
     for (int i = 0; i < index; i++) {
       printf("%.2f ", dataset_array[i]);
     }
   
     if (strcmp(user_command, "add") == 0) {
+      // Input numbers to make array.
+      while (1) {
+        printf("Enter a number or 'x' to submit): ");
+        scanf("%s", input);
+        if (input [0] == 'x') {
+          break;
+        }
+        float f = strtof(input, NULL);
+        dataset_array = realloc(dataset_array, sizeof(float) * (index + 1));
+        dataset_array[index] = f;
+        index++;
+      }
+      // Begin calculation.
       float sum = add(dataset_array, index);
       printf("Sum = %f\n", sum);
     }
     else if (strcmp(user_command, "subtract") == 0) {
+      // Input numbers to make array.
+      while (1) {
+        printf("Enter a number or 'x' to submit): ");
+        scanf("%s", input);
+        if (input [0] == 'x') {
+          break;
+        }
+        float f = strtof(input, NULL);
+        dataset_array = realloc(dataset_array, sizeof(float) * (index + 1));
+        dataset_array[index] = f;
+        index++;
+      }
+      // Begin calculation.
       float difference = subtract(dataset_array, index);
       printf("Difference = %f\n", difference);
     }
     else if (strcmp(user_command, "multiply") == 0) {
+      // Input numbers to make array.
+      while (1) {
+        printf("Enter a number or 'x' to submit): ");
+        scanf("%s", input);
+        if (input [0] == 'x') {
+          break;
+        }
+        float f = strtof(input, NULL);
+        dataset_array = realloc(dataset_array, sizeof(float) * (index + 1));
+        dataset_array[index] = f;
+        index++;
+      }
+      // Begin calculation.
       float product = multiply(dataset_array, index);
       printf("Product = %f\n", product);
     }
     else if (strcmp(user_command, "divide") == 0) {
+      // Input numbers to make array.
+      while (1) {
+        printf("Enter a number or 'x' to submit): ");
+        scanf("%s", input);
+        if (input [0] == 'x') {
+          break;
+        }
+        float f = strtof(input, NULL);
+        dataset_array = realloc(dataset_array, sizeof(float) * (index + 1));
+        dataset_array[index] = f;
+        index++;
+      }
+      // Begin calculation.
       float quotient = divide(dataset_array, index);
       printf("Quotient = %f\n", quotient);
     }
     else if (strcmp(user_command, "mean") == 0) {
+      // Input numbers to make array.
+      while (1) {
+        printf("Enter a number or 'x' to submit): ");
+        scanf("%s", input);
+        if (input [0] == 'x') {
+          break;
+        }
+        float f = strtof(input, NULL);
+        dataset_array = realloc(dataset_array, sizeof(float) * (index + 1));
+        dataset_array[index] = f;
+        index++;
+      }
+      // Begin calculation.
       float xmean = mean(dataset_array, index);
       printf("Quotient = %f\n", xmean);
     }
     else if (strcmp(user_command, "variance") == 0) {
+      // Input numbers to make array.
+      while (1) {
+        printf("Enter a number or 'x' to submit): ");
+        scanf("%s", input);
+        if (input [0] == 'x') {
+          break;
+        }
+        float f = strtof(input, NULL);
+        dataset_array = realloc(dataset_array, sizeof(float) * (index + 1));
+        dataset_array[index] = f;
+        index++;
+      }
+      // Begin calculation.
       printf("Please enter a correction value (0, 1, or more)\n");
       int correction;
       scanf("%d", &correction);
@@ -71,6 +137,19 @@ int main(void) {
       printf("Variance = %f\n", xvariance);
     }
     else if (strcmp(user_command, "standard_deviation") == 0) {
+      // Input numbers to make array.
+      while (1) {
+        printf("Enter a number or 'x' to submit): ");
+        scanf("%s", input);
+        if (input [0] == 'x') {
+          break;
+        }
+        float f = strtof(input, NULL);
+        dataset_array = realloc(dataset_array, sizeof(float) * (index + 1));
+        dataset_array[index] = f;
+        index++;
+      }
+      // Begin calculation.
       printf("Please enter a correction value (0, 1, or more)\n");
       int correction;
       scanf("%d", &correction);
@@ -78,6 +157,19 @@ int main(void) {
       printf("Standard Deviation = %f\n", xstandard_deviation);
     }
     else if (strcmp(user_command, "coefficient_of_variation") == 0) {
+      // Input numbers to make array.
+      while (1) {
+        printf("Enter a number or 'x' to submit): ");
+        scanf("%s", input);
+        if (input [0] == 'x') {
+          break;
+        }
+        float f = strtof(input, NULL);
+        dataset_array = realloc(dataset_array, sizeof(float) * (index + 1));
+        dataset_array[index] = f;
+        index++;
+      }
+      // Begin calculation.
       printf("Please enter a correction value (0 for population or 1 for sample)\n");
       int correction;
       scanf("%d", &correction);
@@ -87,6 +179,19 @@ int main(void) {
       printf("Coefficient of Variation = %f\n", xcoefficient_of_variation);
     }
     else if (strcmp(user_command, "skewness") == 0) {
+      // Input numbers to make array.
+      while (1) {
+        printf("Enter a number or 'x' to submit): ");
+        scanf("%s", input);
+        if (input [0] == 'x') {
+          break;
+        }
+        float f = strtof(input, NULL);
+        dataset_array = realloc(dataset_array, sizeof(float) * (index + 1));
+        dataset_array[index] = f;
+        index++;
+      }
+      // Begin calculation.
       printf("Please enter a correction value (0 for population or 1 for sample)\n");
       int correction;
       scanf("%d", &correction);
@@ -94,6 +199,10 @@ int main(void) {
       printf("Standard Deviation = %f.2\n", xstandard_deviation);
       float xskewness = skewness(dataset_array, correction, index);
       printf("Skewness = %f\n", xskewness);
+    }
+    else if (strcmp(user_command, "covariance") == 0) {
+      printf("Please enter the number of variables to compute:\n");
+      
     }
     else {
       printf("Error. Invalid command.");
